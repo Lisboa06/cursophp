829b8af35eda53af87f0e5c6fa0fc5edf1cb651c
Tiago Lisboa <129691470+Lisboa06@users.noreply.github.com>
2024-01-13 22:03:57 -0300

    Update index.php

--- ex002/index.php
+++ ex002/index.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>Exemplo PHP</h1>
     <?php
         date_default_timezone_set("America/Sao_Paulo");
-        echo "Hoje é dia:" . date("/D/M/Y");
-        echo "A hora atual é:" . date("/G/i/s T");
+        echo "Hoje é dia: " . date("/D/M/Y");
+        echo "<br>";
+        echo "A hora atual é: " . date("/G/i/s T");
+
     ?>
 </body>
 </html>
