73b2f07cbb581743bce650509c635e8525864a7e
Tiago Lisboa <129691470+Lisboa06@users.noreply.github.com>
2024-01-29 17:55:48 -0300

    criei o desafio 004 (incompleto)

--- desafios/d003/conversor.php
+++ desafios/d003/conversor.php
@@ -1,3 +1,23 @@
-<?php 
-    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php
+        $real = 1000;
+        $cotacao = 4.95;
+        $dolar = $real / $cotacao;
+        
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD") . "</p>";
+        ?>
+        <button onclick="javascript:history.back(-1)">Voltar</button>
+    </main>
+</body>
+</html>
---
+++ desafios/d004/teste.php
@@ -0,0 +1,10 @@
+<!-- <pre> -->
+    <?php
+    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'01-25-2024\'&@dataFinalCotacao=\'01-29-2024\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao
+    ';
+    
+    $dados = json_decode(file_get_contents($url), true);
+    
+    var_dump($dados)
+    ?>
+<!-- </pre> -->
