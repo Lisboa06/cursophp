d9d84a6dea2e34481d1b8686072b4aa07d0d041f
Tiago Lisboa <129691470+Lisboa06@users.noreply.github.com>
2024-01-31 12:04:31 -0300

    erro no código d004  corrigido
    havia um espaçamento da url da api

--- desafios/d004/teste.php
+++ desafios/d004/teste.php
@@ -1,10 +1,11 @@
-<!-- <pre> -->
+<pre>
     <?php
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'01-25-2024\'&@dataFinalCotacao=\'01-29-2024\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao
-    ';
+    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'01-26-2024\'&@dataFinalCotacao=\'01-31-2024\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
     
     $dados = json_decode(file_get_contents($url), true);
-    
-    var_dump($dados)
+
+    $cotação = $dados["value"][0]["cotacaoCompra"];
+
+    echo "A cotação foi: $cotação";
     ?>
-<!-- </pre> -->
+</pre>
